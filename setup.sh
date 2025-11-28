1425074d942414658b7e5920253a1168d23936c2
smford <stephen.ford@digital.cabinet-office.gov.uk>
2025-11-28 14:01:54 +0000

    Update setup script
    - added prompting
    - updated readme
    - passed shellcheck checks

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
-echo "Ask for work email address and github username"
+read -rp "What is your digital.cabinet-office.gov.uk email address: " emailaddress
+read -rp "What is your github username: " githubusername
 
 echo "Configuring git"
 git config --global alias.unstage "reset HEAD --"
 git config --list | grep alias
 git config --global commit.gpgsign true
-git config --global user.email "your.name@digital.cabinet-office.gov.uk"
-git config --global user.name "your_github_username"
+git config --global user.email "$emailaddress"
+git config --global user.name "$githubusername"
 
 echo "Configuring gds-cli"
-gds-cli config email your.name@digital.cabinet-office.gov.uk
+gds-cli config email "$emailaddress"
